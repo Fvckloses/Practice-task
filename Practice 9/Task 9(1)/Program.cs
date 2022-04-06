e9d97746936a5ecb7d02d5b1c4b2d12d1c1d8ff8
Danya <dbosatsky@gmail.com>
2022-04-06 09:44:11 +0300

    Add Practice 9

---
+++ Practice 9/Task 9(1)/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Task_9_1_
+{
+    internal class Program
+    {
+        interface Ix
+        {
+            void IxF0();
+            void IxF1();
+        }
+        interface Iy
+        {
+            void F0();
+            void F1();
+        }
+        interface Iz
+        {
+            void F0();
+            void F1();
+        }
+        class TestClass : Ix, Iy, Iz
+        {
+            string text = "";
+            public void F0()
+            {
+                Console.WriteLine("*");
+            }
+
+            public void F1()
+            {
+                Console.WriteLine("5");
+            }
+
+            public void IxF0()
+            {
+                Console.WriteLine(text.ToLower());
+            }
+
+            public void IxF1()
+            {
+                Console.WriteLine(text.ToUpper());
+            }
+
+            public void metod()
+            {
+                Console.Write("Введите символ: ");
+                text = Console.ReadLine();
+                if (text == "*")
+                {
+                    F0();
+                }
+                else
+                {
+                    F1();
+                }
+                Console.Write("Преобразование в нижний регистр: ");
+                IxF0();
+                Console.Write("Преобразование в верхний регистр: ");
+                IxF1();
+            }
+        }
+        static void Main(string[] args)
+        {
+            TestClass test = new TestClass();
+            test.metod();
+        }
+    }
+}
