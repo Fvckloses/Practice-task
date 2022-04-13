53ce8a015cb40dd73d9bc0a57bcb64a4a7c65bea
Danya <dbosatsky@gmail.com>
2022-04-13 12:31:09 +0300

    Add Practice 15

---
+++ Practice 15/Task 15(1)/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task_15_1_
+{
+    class MyArray<T>
+    {
+        public void Add(T value)
+        {
+            Values[index] = value;
+            index++;
+        }
+        public void ShowValues()
+        {
+            for (int i=0; i < index; i++)
+                Console.WriteLine(Values[i]);
+        }
+        public void ShowValue()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Values[a]);
+        }
+        protected int index = 0;
+        protected T[] Values = new T[200];
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray<string> arr1 = new MyArray<string>();
+            Console.Write("Введите количество значений: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Введите число: ");
+                arr1.Add(Console.ReadLine());
+            }
+            arr1.ShowValues();
+            Console.WriteLine($"Введите элемент с 0 до {n-1}");
+            arr1.ShowValue();
+        }
+    }
+}
---
+++ Practice 15/Task 15(2)/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Task_15_2_
+{
+    class MyDictionary<Tkey, Tvalue>
+    {
+        public void Add(Tkey value1, Tkey value2)
+        {
+            Values[index, index1] = value1;
+            index++;
+            index1++;
+        }
+        public void ShowValues()
+        {
+            for (int i = 0; i < index; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                { 
+                    Console.Write(Values[i, j]);
+                }
+                Console.WriteLine();
+            }     
+        }
+        public void ShowValue()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Values[a,b]);
+        }
+        protected int index = 0;
+        protected int index1 = 0;
+        protected Tkey[,] Values = new Tkey[200,200];
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<double, double> arr1 = new MyDictionary<double, double>();
+            Console.Write("Введите количество значений: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a, b;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Введите числа: ");
+                a = Convert.ToInt32(Console.ReadLine());
+                b = Convert.ToInt32(Console.ReadLine());
+                arr1.Add(a, b);
+            }
+            arr1.ShowValues();
+            Console.WriteLine("Введите координаты: ");
+            arr1.ShowValue();
+            Console.ReadLine();
+        }
+    }
+}
