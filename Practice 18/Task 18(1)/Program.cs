bdd603884e0a702b646b099bdec08b07ae0f39eb
Danya <dbosatsky@gmail.com>
2022-04-19 09:01:59 +0300

    Add Practice 18

---
+++ Practice 18/Task 18(1)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_18_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "abc#d##c";
+            Stack<char> stack = new Stack<char>();
+            foreach (var c in a)
+            {
+                if (c == '#')
+                {
+                    if (stack.Count > 0)
+                        stack.Pop();
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+            var array = stack.ToArray();
+            Array.Reverse(array);
+            string s = new string(array);
+            Console.WriteLine(s);
+        }
+    }
+}
+
---
+++ Practice 18/Task 18(2)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Task_18_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2, b = 6;
+            Queue<int> fromAToB = new Queue<int>();
+            Queue<int> lessThenA = new Queue<int>();
+            Queue<int> moreThenB = new Queue<int>();
+            int number;
+            using (StreamReader reader = new StreamReader(@"18.txt", Encoding.Default))
+            {
+                while (!reader.EndOfStream)
+                {
+                    number = int.Parse(reader.ReadLine());
+                    if (number < a)
+                        lessThenA.Enqueue(number);
+                    else if (number > b)
+                        moreThenB.Enqueue(number);
+                    else
+                        fromAToB.Enqueue(number);
+                }
+            }
+            Console.WriteLine(string.Join(" ", fromAToB));
+            Console.WriteLine(string.Join(" ", lessThenA));
+            Console.WriteLine(string.Join(" ", moreThenB));
+        }
+    }
+}
