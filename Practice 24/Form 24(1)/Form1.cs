9edc687b506e64a8d7f16589b3cda025e2248cf1
Danya <dbosatsky@gmail.com>
2022-04-25 09:41:46 +0300

    Add Practice 24

---
+++ Practice 24/Form 24(1)/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_24_1_
+{
+    public partial class Form1 : Form
+    {
+        private int openDocuments;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Tag.ToString())
+            {
+                case "NewDoc":
+                    ChildForm newChild = new ChildForm();
+                    newChild.MdiParent = this;
+                    newChild.Show();
+                    newChild.Text = newChild.Text + " " +
+                    ++openDocuments;
+                    break;
+                case "Cascade":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+                    break;
+                case "Title":
+                    this.LayoutMdi
+                    (System.Windows.Forms.MdiLayout.TileHorizontal);
+                    break;
+            }
+        }
+    }
+}
