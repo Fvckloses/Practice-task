ad4e76ff0670c4a1e1f241eb4d05089888f35cdf
Danya <dbosatsky@gmail.com>
2022-04-29 22:16:20 +0300

    Add Practice 28

---
+++ Practice 28/Form 28/TestDrawing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Form_28
+{
+    internal class TestDrawing : Control
+    {
+        protected override void OnRender(System.Windows.Media.DrawingContext drawingContext)
+        {
+            drawingContext.DrawLine(new Pen(Brushes.Red, 2),
+                new Point(0, 0),
+                new Point(this.Height, this.Width));
+
+            drawingContext.DrawLine(new Pen(Brushes.Red, 2),
+                new Point(0, this.Width),
+                new Point(this.Height, 0));
+
+            base.OnRender(drawingContext);
+        }
+    }
+
+}
