d7e47e690df3fefb6a07655192fffe45b4d9e8fd
Danya <dbosatsky@gmail.com>
2022-05-04 14:14:26 +0300

    Add Practice 27

---
+++ Practice 27/Form 27(1)/InterFaces/IXmlWorker.cs
@@ -0,0 +1,18 @@
+﻿using Form_26_1_.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Form_26_1_.InterFaces
+{
+    public interface IXmlWorker
+    {
+        void Load(string xmlDocPath);
+        void Add(Parking parking);
+        void Delete(string mark);
+        Parking FindBy(string mark);
+        List<Parking> GetAll();
+    }
+}
